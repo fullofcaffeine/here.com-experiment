fc56fba1340526a06c0a25fb53b891e849b3fdbc
Marcelo de Moraes Serpa <boss@fullofcaffeine.com>
2015-07-14 18:32:06 -0500

    Small tool to export data in the json format

---
+++ exportData.js
@@ -0,0 +1,36 @@
+var pg = require('pg');
+var fs = require('fs');
+var conString = "postgres://realinsights:realinsights@localhost/realinsights_development";
+var ws = fs.createWriteStream('listings.json');
+var collection = [];
+var count = 0;
+
+pg.connect(conString, function(err, client, done) {
+  if (err) {
+    return console.error('error fetching client from pool', err);
+  }
+  var total = 0;
+
+  var query = client.query("SELECT lat, lon, city, local_price FROM listings WHERE city =  'Rio de Janeiro'");
+  query.on('row', function(row) {
+    var listing = {
+      "lat": row.lat,
+      "lon": row.lon,
+      "city": row.city,
+      "value": row.local_price
+    };
+
+    count++;
+    console.log('New row ' + count + ' fetched...');
+    collection.push(listing);
+  });
+
+  query.on('end', function(result) {
+    console.log('saving...');
+    ws.write(JSON.stringify(collection));
+    ws.close();
+    process.exit();
+  });
+});
+
+
